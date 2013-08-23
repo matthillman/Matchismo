7228a327cbabd0f94eae9aa49ec4bf7dc726a63c
matthillman <matt@Matthews-MacBook-Air.local>
2013-08-22 20:25:06 -0500

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Matthew Hillman on 8/22/13.
+//  Copyright (c) 2013 Matthew Hillman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Matthew Hillman on 8/22/13.
+//  Copyright (c) 2013 Matthew Hillman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
